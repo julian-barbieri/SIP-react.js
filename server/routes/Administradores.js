6544ec272a857dfc073dc5ed8d82ac2d790fcd9c
julianbarbieri1 <j.barbieri@usal.edu.ar>
2024-06-26 15:57:24 -0300

    bcrypt

--- server/routes/Administradores.js
+++ server/routes/Administradores.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Administradores } = require('../models');
+const bcrypt = require('bcrypt');
 
 //get all
 router.get("/", async (req, res) =>{
@@ -15,16 +16,38 @@ router.get("/adminByUsername/:nombre_usuario", async (req, res) =>{
     res.json(admin);
 });
 
-//login user 
+//login administradores 
 router.post('/login',async(req,res)=>{
-    const user = await Administradores.findOne({ 
-        where : {nombre_usuario : req.body.nombre_usuario, contraseña: req.body.contraseña }});
-    res.json(user);
+    
+    const {nombre_usuario, contraseña} = req.body;
+
+    const username = await Administradores.findOne({ 
+        where : {
+            nombre_usuario: req.body.nombre_usuario 
+        }
+    });
+    if(!username){
+        res.send("wrong username")
+        return
+    }
+    const isValid = await bcrypt.compare(contraseña, username.contraseña);
+    if(!isValid){
+        res.send("wrong password")
+        return
+    }
+    res.json(username);
 });
 
 //create a new Administrador
 router.post("/", async (req, res) =>{
-    const Administrador = req.body;
+    //const Administrador = req.body;
+    const {nombre_completo, nombre_usuario, contraseña} = req.body;
+    const hash  = await bcrypt.hash(contraseña, 10);
+    const Administrador = {
+        nombre_completo,
+        nombre_usuario,
+        contraseña: hash,
+    }
     await Administradores.create(Administrador);
     res.json(Administrador);
 })
